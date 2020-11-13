10a38f7775bb6ca268b6fee8e162c55e8e578f84
Kamil Wator <kamil.w.ofi@gmail.com>
2020-11-13 17:15:55 +0100

    Edycja 1

---
+++ Main.java
@@ -0,0 +1,50 @@
+public static void main(String[] args) {
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
+        }
+
+class Person {
+    private final String firstName;
+    private final String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getlastName() {
+        return lastName;
+    }
+}
+
+class PeopleStats {
+    private final List<Person> people;
+
+    public PeopleStats(Path inputFilePath) {
+        try {
+            people = Files.lines(inputFilePath)
+                    .map(line -> line.split("\t"))
+                    .map(chunks -> new Person(chunks[0], chunks[1]))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public long count() {
+        return people.size();
+    }
+    public long countUniqueLastNames() {
+        return people.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .count();
+    }
+
+}
+
